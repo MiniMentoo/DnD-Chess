e2b84fe727c5efdc0e0fe5bd98617ee6738aa6c7
Mostafa A <mostafaalkhazaali@gmail.com>
2024-04-13 00:36:36 +0100

    Added command that displayes the users current hand in the server.

---
+++ commands/games/hand.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, codeBlock} = require('discord.js');
+
+module.exports = {
+    data : new SlashCommandBuilder()
+        .setName('hand')
+        .setDescription('shows you your hand'),
+    async execute(interaction) {
+        let reply = `temp`;
+        if (global.hands.has(interaction.guild.id)){
+            let serverHands = global.hands.get(interaction.guild.id);
+            if (serverHands.has(interaction.user)) {
+                let userHand = serverHands.get(interaction.user);
+                reply = codeBlock('md', `Your hand currently has
+${userHand}`);
+            } else {
+                reply = {content : `You don't have a hand in this server yet! Try joining a game with /join`, ephemeral : false};
+            }
+        } else {
+            reply = {content : `This server doesn't have a game yet! Try making one with /join`, ephemeral : true};
+        }
+        await interaction.reply(reply);
+    },
+};
