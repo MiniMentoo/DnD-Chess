f4ab9017d34feeeb04efa2b0ddd539fb5c18fca6
MiniMento <zkac192@live.rhul.ac.uk>
2024-06-07 21:45:40 +0100

    Modified message if /hand is called while having an empty hand.

--- commands/games/hand.js
+++ commands/games/hand.js
@@ -10,8 +10,11 @@ module.exports = {
             let serverHands = global.hands.get(interaction.guild.id);
             if (serverHands.has(interaction.user)) {
                 let userHand = serverHands.get(interaction.user);
-                reply = codeBlock('md', `Your hand currently has
-${userHand}`);
+                if (userHand.length == 0){
+                    reply = codeBlock('md', `Your hand is currently empty!`)
+                } else {
+                    reply = codeBlock('md', `Your hand currently has
+${userHand}`);}
             } else {
                 reply = {content : `You don't have a hand in this server yet! Try joining a game with /join`, ephemeral : false};
             }
